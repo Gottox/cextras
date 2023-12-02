e56e9d78af50c14ad33f08573162606ecb5590ed
Enno Boland <g@s01.de>
2023-12-02 19:58:05 +0100

    collector: fix code smell issues on sonarcloud

--- lib/collection/collector.c
+++ lib/collection/collector.c
@@ -6,11 +6,68 @@
 
 #include "../../include/cextras/collection.h"
 #include "../../include/cextras/error.h"
+#include <stdint.h>
+#include <sys/wait.h>
+
+static const uintptr_t nullword = 0;
+
+/**
+ *
+ */
+/**
+ * @brief Handles the next element in the collection.
+ *
+ * This function retrieves the next element using the provided iterator and
+ * appends information about the element to the specified lists.
+ *
+ * @param next          The callback function for obtaining the next element
+ * from the iterator.
+ * @param iterator      The iterator used to traverse the collection.
+ * @param list          The buffer to store offset of the values.
+ * @param list_values   The buffer to store values.
+ *
+ * @retval              1 if a new value was added
+ * @retval              0 if the iterator ended
+ * @retval              negative if an error occured.
+ */
+static int
+handle_next(
+		cx_collector_next_t next, void *iterator, struct CxBuffer *list,
+		struct CxBuffer *list_values) {
+	int rv;
+	const char *value = NULL;
+	size_t size = 0;
+
+	rv = next(iterator, &value, &size);
+	if (rv < 0) {
+		return rv;
+	} else if (value == NULL) {
+		return 0;
+	}
+
+	size_t offset = cx_buffer_size(list_values);
+	char *offset_ptr = (void *)offset;
+	rv = cx_buffer_append(
+			list, (const uint8_t *)&offset_ptr, sizeof(uintptr_t));
+	if (rv < 0) {
+		return rv;
+	}
+	rv = cx_buffer_append(list_values, (const uint8_t *)value, size);
+	if (rv < 0) {
+		return rv;
+	}
+	rv = cx_buffer_append(
+			list_values, (const uint8_t *)&nullword, sizeof(char));
+	if (rv < 0) {
+		return rv;
+	}
+
+	return 1;
+}
 
 int
 cx_collect(char ***target, cx_collector_next_t next, void *iterator) {
 	int rv = 0;
-	static const uintptr_t nullptr = 0;
 	struct CxBuffer list = {0};
 	struct CxBuffer list_values = {0};
 	size_t elements = 0;
@@ -26,35 +83,18 @@ cx_collect(char ***target, cx_collector_next_t next, void *iterator) {
 	}
 
 	for (;;) {
-		const char *value = NULL;
-		size_t size = 0;
-
-		rv = next(iterator, &value, &size);
-		if (rv < 0) {
-			goto out;
-		}
-		if (value == NULL) {
+		rv = handle_next(next, iterator, &list, &list_values);
+		if (rv > 0) {
+			elements++;
+		} else {
 			break;
 		}
-
-		size_t index = cx_buffer_size(&list_values);
-		char *index_ptr = (void *)index;
-		elements++;
-		rv = cx_buffer_append(&list, (uint8_t *)&index_ptr, sizeof(char *));
-		if (rv < 0) {
-			goto out;
-		}
-		rv = cx_buffer_append(&list_values, (uint8_t *)value, size);
-		if (rv < 0) {
-			goto out;
-		}
-		rv = cx_buffer_append(&list_values, (uint8_t *)&nullptr, sizeof(char));
-		if (rv < 0) {
-			goto out;
-		}
+	}
+	if (rv < 0) {
+		goto out;
 	}
 
-	rv = cx_buffer_append(&list, (uint8_t *)&nullptr, sizeof(char *));
+	rv = cx_buffer_append(&list, (const uint8_t *)&nullword, sizeof(char *));
 	if (rv < 0) {
 		goto out;
 	}
